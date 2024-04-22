975f59d779d2fc687a4fb1b1957ec8f95e70c805
harshkumar1302 <bs824536@gmail.com>
2024-04-22 21:36:07 +0530

    custom demo react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+
+// function myApp() {
+//   return (
+//     <h2>Hello Guzs</h2>
+//   )
+// }
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   myApp(),
+// )
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+
+
+
+
